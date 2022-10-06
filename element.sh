ae66a0583b9528f39664c7d6bfa7121fcc3bb4f5
chemikas <chemikas@yahoo.com>
2022-10-06 13:39:04 +0000

    feat: added get element data

--- element.sh
+++ element.sh
@@ -10,10 +10,35 @@ if [[ -z $1 ]]
     then 
       echo "It's not a number"
     else 
-      echo "It's a number!"
+      
+      NAME=$($PSQL "SELECT name from elements where atomic_number = $1;")
+      SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $1;")
+      ATOMIC_NUMBER=$1
+      echo "The element with atomic number $ATOMIC_NUMBER is$NAME (${SYMBOL})."
+      GET_PROPS
     fi
+
     #check if argument is one or two leters
 
     #check if arguent is in list of elements name
     
   fi
+
+#get element data
+echo " DON'T WORRY, BE HAPPY ! ! ! ! "
+:'
+_elements
+atomic_number
+name
+symbol
+_types
+type
+_properties
+atomic_weight
+melting_point_celsius
+boiling_point_celsius
+'
+
+#print report
+echo -e "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. 
+Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
