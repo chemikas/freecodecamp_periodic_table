80a4428a99aea0192f77a1c09d4ce1180781e34f
chemikas <chemikas@yahoo.com>
2022-10-06 11:46:10 +0000

    refactor: order of if close changed

--- element.sh
+++ element.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-if [[ $1 ]]
+if [[ -z $1 ]]
   then
-    echo -e "\n$1"
+      echo "Please provide an element as an argument."
   else 
-    echo "Please provide an element as an argument."
+    # check if argument is a number
+    if [[ ! $1 =~ ^[0-9]+$ ]]
+    then 
+      echo "It's not a number"
+    else 
+      echo "It's a number!"
+    fi
+    #check if argument is one or two leters
+
+    #check if arguent is in list of elements name
+    
   fi
-
-
-# check if argument is a number
-if [[ ! $1 =~ ^[0-9]+$ ]]
-then 
-  echo "It's not a number"
-else 
-  echo "It's a number!"
-fi
-#check if argument is one or two leters
-
-#check if arguent is in list of elements name
