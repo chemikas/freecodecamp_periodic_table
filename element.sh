1b196d7cdbd56465cc252ba1928ba4e75261874c
chemikas <chemikas@yahoo.com>
2022-10-06 11:22:49 +0000

    fix: added bin/bash and psql call

--- element.sh
+++ element.sh
@@ -1,14 +1,21 @@
-echo "Please provide an element as an argument."
-MAIN_MENU() {
+#!/bin/bash
+PSQL = "psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
 if [[ $1 ]]
   then
     echo -e "\n$1"
+  else 
+    echo "Please provide an element as an argument."
   fi
-}
+
 
 # check if argument is a number
-if [[ $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
-
+if [[ ! $1 =~ ^[0-9]+$ ]]
+then 
+  echo "It's not a number"
+else 
+  echo "It's a number!"
+fi
 #check if argument is one or two leters
 
 #check if arguent is in list of elements name
