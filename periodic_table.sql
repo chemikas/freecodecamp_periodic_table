b59d841bcf40a3ae57276e681343f67294fd2a2b
chemikas <chemikas@yahoo.com>
2022-10-07 09:35:38 +0000

    deleted Mt

--- periodic_table.sql
+++ periodic_table.sql
@@ -96,7 +96,6 @@ INSERT INTO public.elements VALUES (5, 'B', 'Boron');
 INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
-INSERT INTO public.elements VALUES (1000, 'Mt', 'moTanium');
 INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
 INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
@@ -112,7 +111,6 @@ INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 2);
 INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 2);
 INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, 2);
 INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, 2);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
 INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 3);
 INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 2);
 INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 2);
