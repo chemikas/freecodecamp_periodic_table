ff2f1ee5b24360d64dc707dce086f9fdb25b0c0f
chemikas <chemikas@yahoo.com>
2022-10-07 06:06:56 +0000

    fix: it then clause fixed

--- element.sh
+++ element.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+
+
 if [[ -z $1 ]]
   then
       echo "Please provide an element as an argument."
   else 
     # check if argument is a number
-    if [[ ! $1 =~ ^[0-9]+$ ]]
+    if [[ $1 =~ ^[0-9]+$ ]]
     then 
-      echo "It's not a number"
-    else 
-      
+         
       NAME=$($PSQL "SELECT name from elements where atomic_number = $1;")
       SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $1;")
       ATOMIC_NUMBER=$1
